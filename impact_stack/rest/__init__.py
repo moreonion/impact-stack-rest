8682dfa683c2177a5819e88da2b07d3aba382535
Roman Zimmermann <roman@more-onion.com>
2023-10-02 10:02:23 +0200

    feat: Auto-renew tokens

--- impact_stack/rest/__init__.py
+++ impact_stack/rest/__init__.py
@@ -1,10 +1,11 @@
 """Useful wrappers for the requests library."""
+import datetime
 import posixpath
 import urllib.parse
 
 import requests
 
-from impact_stack.rest import rest
+from impact_stack.rest import rest, utils
 
 try:
     import flask
@@ -15,26 +16,46 @@ except ImportError:  # pragma: no cover
 class AuthMiddleware:
     """Requests middleware for authenticating using JWT tokens.
 
-    The middleware transparently requests an API-token from the auth-app on-demand.
+    The middleware transparently requests an API-token from the auth-app during the first request.
+    Afterwards each request gets the token added to its headers.
+
+    A new token is requested on-demand whenever the old token has (almost) expired.
     """
 
-    # pylint: disable=too-few-public-methods
-    # For now the middleware is very simple, but in the future it should also take care of caching
-    # the JWT and renew if needed.
-
-    def __init__(self, client, api_key):
+    def __init__(self, client, api_key, token_life_time_buffer):
         """Create new auth-app requests auth middleware."""
         self.client = client
         self.api_key = api_key
+        self.life_time_buffer = token_life_time_buffer
+        self.token = None
+        self.expires_at = None
+
+    def needs_refresh(self):
+        """Check if we have a token and it can still be used."""
+        if not self.token:
+            return True
+        time_left = self.expires_at - utils.now().timestamp()
+        return time_left < self.life_time_buffer
 
     def get_token(self):
         """Use the API key to get a JWT."""
-        return self.client.post("token", json=self.api_key, json_response=True)["token"]
+        if self.needs_refresh():
+            data = self.client.post("token", json=self.api_key, json_response=True)
+            self.token = data["token"]
+            self.expires_at = data["data"]["exp"]
+        return self.token
 
     def __call__(self, request: requests.PreparedRequest):
         """Add the JWT token to the request."""
         request.headers["Authorization"] = "Bearer " + self.get_token()
         return request
+
+
+def timeout_sum(timeout):
+    """Sum up all the values in a requests timeout."""
+    # A timeout is a tuple of connect and read timeout. Passing an integer is a short hand for
+    # using the same number for both.
+    return timeout * 2 if isinstance(timeout, int) else sum(timeout)
 
 
 class ClientFactory:
@@ -57,7 +78,12 @@ class ClientFactory:
     def __init__(self, base_url, api_key):
         """Create a new client factory instance."""
         self.base_url = base_url
-        self.auth_middleware = AuthMiddleware(self.get_client("auth", needs_auth=False), api_key)
+        auth_client = self.get_client("auth", needs_auth=False)
+        self.auth_middleware = AuthMiddleware(
+            auth_client,
+            api_key,
+            timeout_sum(auth_client.request_timeout),
+        )
 
     def get_client(self, app_slug, api_version=None, needs_auth=True):
         """Get a new API client for an Impact Stack service."""
